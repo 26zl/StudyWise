ca5b3ff9d79b856c4766e647a9ea1c20439b38cb
Laurent Zogaj <143036376+26zl@users.noreply.github.com>
2026-01-26 14:13:38 +0100

    Konfig endring, la til turbopack for å finne lock root pnpm riktig og fjerne warnings

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,16 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["common"],
   output: "standalone",
+  turbopack: {
+    root: path.resolve(__dirname, ".."),
+  },
   async rewrites() {
     // Bruker miljøvariabel for API URL
     const apiUrl = process.env.NEXT_PUBLIC_API_URL;
